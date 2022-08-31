d0e178b25fd94099730aa9c3973d4604d9fe08d4
dineka89 <dineka89@inbox.ru>
2022-08-31 16:07:33 +0300

    переделала P.P 2.1.2

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,9 +2,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+
 @Configuration
 public class AppConfig {
- 
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
